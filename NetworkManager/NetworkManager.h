20e7c52a75a7e17e5fef0d8cad3c44c7b0443b60
mbyk <marubayashi@gmail.com>
2015-08-29 20:08:58 +0900

    SessionManagerを継承

---
+++ NetworkManager/NetworkManager.h
@@ -0,0 +1,14 @@
+//
+//  NetworkManager.h
+//  NetworkManager
+//
+//  Created by maru on 2015/08/29.
+//  Copyright (c) 2015年 maru. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "AFHTTPSessionManager.h"
+
+@interface NetworkManager : AFHTTPSessionManager
+
+@end
---
+++ NetworkManager/NetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.m
+//  NetworkManager
+//
+//  Created by maru on 2015/08/29.
+//  Copyright (c) 2015年 maru. All rights reserved.
+//
+
+#import "NetworkManager.h"
+
+@implementation NetworkManager
+
+@end
